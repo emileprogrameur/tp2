7ef1920ba972670d6e1c8634e69673913194da53
Mathieu Malette <matt_malette_666@hotmail.com>
2018-02-27 15:40:03 -0500

    exercice 8

--- 01_app.js
+++ 01_app.js
@@ -5,13 +5,21 @@ const app = express();
 const bodyParser= require('body-parser');
 const MongoClient = require('mongodb').MongoClient; // le pilote MongoDB
 const ObjectID = require('mongodb').ObjectID;
+const i18n = require("i18n")
 app.use(bodyParser.urlencoded({extended: true}));
 /* on associe le moteur de vue au module «ejs» */
 app.use(express.static('public'));
 
+i18n.configure({ 
+   
+   locales : ['fr', 'en'],
+   cookie : 'langueChoisie', 
+   directory : __dirname + '/locales' 
+
+})
 
 /* Ajoute l'objet i18n à l'objet global «res» */
-
+app.use(i18n.init);
 
 
 let db // variable qui contiendra le lien sur la BD
@@ -35,6 +43,17 @@ Les routes
 ////////////////////////////////////////// Route /
 app.set('view engine', 'ejs'); // générateur de template
 
+///////////////////////////////////////////
+
+app.get("/:locale(en|fr)", (req,res)=>{
+
+	res.setLocale(req.params.locale)
+	console.log('en= ' + res.__('bonjour'))
+	console.log(res.__("maison"))
+
+	res.render('accueil.ejs')
+
+})
 
 //////////////////////////////////////////
 app.get('/', function (req, res) {
