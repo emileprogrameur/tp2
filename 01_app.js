ecd9a97ca3999500175947b3e547d2dc0a993cfa
emileprogrameur <emile_bisaillon@hotmail.fr>
2018-03-19 18:41:31 -0400

    ajout du chat en socket

--- 01_app.js
+++ 01_app.js
@@ -1,62 +1,43 @@
-const express = require('express');
-const fs = require('fs');
-const util = require("util");
-const app = express();
-
-const server = require('http').createServer(app);
-const io = require('./mes_modules/chat_socket').listen(server);
-
-const bodyParser= require('body-parser');
-const MongoClient = require('mongodb').MongoClient; // le pilote MongoDB
-const ObjectID = require('mongodb').ObjectID;
+const express = require('express')
+const fs = require('fs')
+const util = require("util")
+const app = express()
+const server = require('http').createServer(app)
+const io = require('./mes_modules/chat_socket').listen(server)
+const bodyParser= require('body-parser')
+const MongoClient = require('mongodb').MongoClient
+const ObjectID = require('mongodb').ObjectID
 const i18n = require("i18n")
 const cookieParser = require('cookie-parser')
 const Peupler = require('./public/data/peupler')
-
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended: true}));
-/* on associe le moteur de vue au module «ejs» */
+app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'));
 
 i18n.configure({ 
-   
    locales : ['fr', 'en'],
    cookie : 'langueChoisie', 
    directory : __dirname + '/locales' 
-
 })
 
 app.use(cookieParser())
+app.use(i18n.init)
 
-/* Ajoute l'objet i18n à l'objet global «res» */
-app.use(i18n.init);
-
-
-
-
-let db // variable qui contiendra le lien sur la BD
+let db
 
 MongoClient.connect('mongodb://127.0.0.1:27017', (err, database) => {
  if (err) return console.log(err)
  db = database.db('carnet_adresse')
+
 console.log('connexion à la BD')
 
-// lancement du serveur Express sur le port 8081
  server.listen(8081, (err) => {
  	if (err) console.log(err)
  console.log('connexion à la BD et on écoute sur le port 8081')
  })
 })
 
-
-/*
-Les routes
-*/
-
-////////////////////////////////////////// Route /
-app.set('view engine', 'ejs'); // générateur de template
-
-///////////////////////////////////////////
+app.set('view engine', 'ejs')
 
 app.get("/:locale(en|fr)", (req,res)=>{
 
@@ -64,14 +45,12 @@ app.get("/:locale(en|fr)", (req,res)=>{
 	res.setLocale(req.params.locale)
 	console.log(res.__('bonjour'))
 	console.log(res.__("maison"))
-
-
-
 	res.render('accueil.ejs')
 
 })
 
-//////////////////////////////////////////
+
+/*------------------------------------Accueil-----------------------------*/
 app.get('/', function (req, res) {
 
  console.log("req.cookies.langueChoisie = " + req.cookies.langueChoisie)
@@ -80,8 +59,8 @@ app.get('/', function (req, res) {
  
   });
 
-//////////////////////////////////////////
 
+/*------------------------------------Modifier-----------------------------*/
 app.post('/ajax_modifier', (req,res) => {
    console.log("La route = /ajax_modifier")
    req.body._id = ObjectID(req.body._id)
@@ -94,8 +73,8 @@ app.post('/ajax_modifier', (req,res) => {
    })
 })
 
-//////////////////////////////////////////
 
+/*------------------------------------Détruire-----------------------------*/
 app.post('/ajax_detruire', (req, res) => {
 	let id = req.body._id
   db.collection('adresse').findOneAndDelete({_id: ObjectID(id)}, (err, resultat) => {
@@ -105,7 +84,8 @@ app.post('/ajax_detruire', (req, res) => {
   })
 })
 
-//////////////////////////////////////////  Route Adresse
+
+/*------------------------------------Adresse-----------------------------*/
 app.get('/adresse', function (req, res) {
    var cursor = db.collection('adresse')
                 .find().toArray(function(err, resultat){
@@ -113,25 +93,25 @@ app.get('/adresse', function (req, res) {
  res.render('adresse.ejs', {adresses: resultat})   
   });
 })
-//////////////////////////////////////////  Route Rechercher
-app.post('/rechercher',  (req, res) => {
 
-})
-////////////////////////////////////////// Route /ajouter
+
+/*------------------------------------Rechercher-----------------------------*/
+app.post('/rechercher',  (req, res) => {})
+
+/*------------------------------------Ajouter-----------------------------*/
 app.post('/ajouter', (req, res) => {
 console.log('route /ajouter')	
  db.collection('adresse').save(req.body, (err, result) => {
- if (err) return console.log(err)
- // console.log(req.body)	
+ if (err) return console.log(err)	
  console.log('sauvegarder dans la BD')
  res.redirect('/adresse')
  })
 })
 
-////////////////////////////////////////  Route /modifier
+
+/*------------------------------------Modifier-----------------------------*/
 app.post('/modifier', (req, res) => {
 console.log('route /modifier')
-// console.log('util = ' + util.inspect(req.body));
 req.body._id = 	ObjectID(req.body._id)
  db.collection('adresse').save(req.body, (err, result) => {
 	 if (err) return console.log(err)
@@ -140,29 +120,24 @@ req.body._id = 	ObjectID(req.body._id)
 	 })
 })
 
-
-////////////////////////////////////////  Route /detruire
 app.get('/detruire/:id', (req, res) => {
  console.log('route /detruire')
- // console.log('util = ' + util.inspect(req.params));	
  var id = req.params.id
  console.log(id)
  db.collection('adresse')
  .findOneAndDelete({"_id": ObjectID(req.params.id)}, (err, resultat) => {
 
 if (err) return console.log(err)
- res.redirect('/adresse')  // redirige vers la route qui affiche la collection
+ res.redirect('/adresse')
  })
 })
 
 
-///////////////////////////////////////////////////////////   Route /trier
+/*------------------------------------Trier-----------------------------*/
 app.get('/trier/:cle/:ordre', (req, res) => {
-
  let cle = req.params.cle
  let ordre = (req.params.ordre == 'asc' ? 1 : -1)
  let cursor = db.collection('adresse').find().sort(cle,ordre).toArray(function(err, resultat){
-
   ordre = (req.params.ordre == 'asc' ? 'desc' : 'asc')  
  res.render('adresse.ejs', {adresses: resultat, cle, ordre })	
 })
@@ -170,9 +145,8 @@ app.get('/trier/:cle/:ordre', (req, res) => {
 }) 
 
 
-/////////////////////////////////////////////////////////  Route /peupler
+/*------------------------------------Vider-----------------------------*/
 app.get('/vider', (req, res) => {
-
 	let cursor = db.collection('adresse').drop((err, res)=>{
 		if(err) console.error(err)
 			console.log('ok')
@@ -181,7 +155,8 @@ app.get('/vider', (req, res) => {
 	res.redirect('/adresse')
 })
 
-//////////////////////////////////////////////////////// Route Peupler
+
+/*----------------------------------Peupler-----------------------------*/
 app.get('/peupler', (req, res) => {
 	let resultat = Peupler();
 	db.collection('adresse').insert(resultat, (err, result) => {
@@ -191,11 +166,8 @@ app.get('/peupler', (req, res) => {
 })
 
 
-//////////////////////////////////////////////////////// Route chat
+/*------------------------------------Chat-----------------------------*/
 app.get('/chat', (req, res) => {
-
-
-	
 	res.render('socket_vue.ejs')
 })
 
